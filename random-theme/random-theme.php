bfe319ec8e0fe8cdaac2607954661e0c55f3b72e
aaron <aaron@f53fe46e-8c02-459b-9fdd-93e75771b278>
2011-01-04 20:32:36 +0000

    1.0.1 - 1/05/2011 ---------------------------------------------------------------------- - WP 3.1 Compatibility - Changed install location for autoupdate ability
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/random-theme@1870 f53fe46e-8c02-459b-9fdd-93e75771b278

--- random-theme/random-theme.php
+++ random-theme/random-theme.php
@@ -3,12 +3,13 @@
 Plugin Name: Random Theme on Signup
 Plugin URI: http://premium.wpmudev.org/project/random-theme-on-signup
 Description: Activates a random theme for new blog signups
-Version: 1.0
+Version: 1.0.1
 Author: Aaron Edwards (Incsub)
 Author URI: http://uglyrobot.com
+Network: true
 WDP ID: 137
 
-Copyright 2009-2010 Incsub (http://incsub.com)
+Copyright 2009-2011 Incsub (http://incsub.com)
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License (Version 2 - GPLv2) as published by
@@ -23,6 +24,11 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 */
+
+//force multisite
+if ( !is_multisite() )
+  exit( __('Random Theme on Signup is only compatible with Multisite installs.') );
+
 
 //------------------------------------------------------------------------//
 //---Hook-----------------------------------------------------------------//
@@ -69,4 +75,15 @@ function random_theme_switch_theme($blog_ID) {
 //---Page Output Functions------------------------------------------------//
 //------------------------------------------------------------------------//
 
+///////////////////////////////////////////////////////////////////////////
+/* -------------------- Update Notifications Notice -------------------- */
+if ( !function_exists( 'wdp_un_check' ) ) {
+  add_action( 'admin_notices', 'wdp_un_check', 5 );
+  add_action( 'network_admin_notices', 'wdp_un_check', 5 );
+  function wdp_un_check() {
+    if ( !class_exists( 'WPMUDEV_Update_Notifications' ) && current_user_can( 'edit_users' ) )
+      echo '<div class="error fade"><p>' . __('Please install the latest version of <a href="http://premium.wpmudev.org/project/update-notifications/" title="Download Now &raquo;">our free Update Notifications plugin</a> which helps you stay up-to-date with the most stable, secure versions of WPMU DEV themes and plugins. <a href="http://premium.wpmudev.org/wpmu-dev/update-notifications-plugin-information/">More information &raquo;</a>', 'wpmudev') . '</a></p></div>';
+  }
+}
+/* --------------------------------------------------------------------- */
 ?>
